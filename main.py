cd8368055164f974278e7b4442ced4097fb7a5d2
ProfitzTV <raygould@gmail.com>
2022-08-06 18:17:12 -0400

    Updated UI. Added drop-down list for actions to add/remove streamer. More status messages based upon actions performed.

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ from yaml.loader import BaseLoader
     Date of Origination:  2022-07-31 | version 0.1
 """
 
-# TODO Add update, delete, clear functionality on streamers list
 # TODO Add an customizable interval timer where the app doesn't make the window unmovable/frozen
 streamers = []
+new_values = []
 headings = ['streamer']
 sg.theme('GreenMono')
 
@@ -30,7 +30,10 @@ def func(window):
 def new_thread(window, event, values):
     global running
     schedule.every(30).seconds.do(func, window=window)
-    running = True
+    if event == '-STOPMON-':
+        running = False
+    elif event == '-STARTMON-':
+        running = True
     while running:
         schedule.run_pending()
         stream_mon()
@@ -40,22 +43,19 @@ def stream_mon():
     run_live_check = heart_beat(streamers)
     run_live_check.sort()
     # change button text to Stop Monitoring
-    window['-STARTMON-'].update(text='Stop Monitoring')
     if len(run_live_check) == 0:
-        window['-STATUSMSG-'].update("No live streamers atm", text_color='red')
+        window['-STATUSMSG-'].update("There are none of your favorite streamers online.", text_color='red')
         window.refresh()
     else:
-        window['-STATUSMSG-'].update("Streamers are live.", text_color='green')
-        window.refresh()
         window['-FRAME_LIVE_TEXT-'].update("")
+        window.refresh()
 
     for streamer in run_live_check:
         # TODO turn these into URL links
         # TODO add their avatar instead of or beside their name?
-        window['-FRAME_LIVE_TEXT-'].update(streamer + "\n", append=True, autoscroll=True,
-                                           text_color='white', background_color_for_value='green')
+        window['-FRAME_LIVE_TEXT-'].update(streamer + "\n", append=True, autoscroll=True, text_color='white', background_color_for_value='green')
     window.refresh()
-    # webbrowser.open('https://www.twitch.tv/' + streamer + '/')
+# webbrowser.open('https://www.twitch.tv/' + streamer + '/')
 
 
 def heart_beat(streamer_list1: list):
@@ -63,10 +63,8 @@ def heart_beat(streamer_list1: list):
     chan_status = []
     for name in streamer_list1:
         checker = is_live(name)
-
         if checker == "live":
             chan_status.append(name)
-
     return chan_status
 
 
@@ -96,25 +94,73 @@ def starting_app():
             running = False
             break
 
-        if event == '-UPDATE-':
-            # take what is listed in the multi-line and overwrite yaml file
-            with open('streamers.yaml', 'w') as f:
-                yaml.dump_all(streamers, f, sort_keys=True)
-            f.close()
-            # TODO provide a status of what happened during the update (example: added 2, or removed x)
+        if event == '-SUBMIT-':
+            # read in the value of the drop down
+            combo = window['-COMBO-'].get()
 
-        if event == '-ADD-':
-            streamers.append(values[0])
-            streamers.sort()
-            window['-MULTI-'].update("\n" + values[0], append=True)
-            window.refresh()
-            with open('streamers.yaml', 'w') as f:
-                yaml.dump_all(streamers, f, sort_keys=True)
-            f.close()
+            # read in the value of the txt box
+            inputtxt = window['-INPUTTXT-'].get()
+            if not inputtxt:
+                window['-STATUSMSG-'].update('Enter a streamer''\'s name before clicking submit.', text_color='red')
+            elif not combo:
+                window['-STATUSMSG-'].update('Choose an option from the drop-down before clicking submit.', text_color='red')
+            else:
+                # perform ADD action
+                if combo == 'ADD':
+                    streamers.sort()
+                    if inputtxt in streamers:
+                        window['-STATUSMSG-'].update(inputtxt + " is already in the file.", text_color='red')
+                    else:
+                        streamers.append(inputtxt)
+                        window['-STATUSMSG-'].update("Added: " + inputtxt, text_color='green')
+                        window['-MULTI-'].update(inputtxt+'\n', append=True)
+                        window.refresh()
+                        with open('streamers.yaml', 'w') as f:
+                            yaml.dump_all(streamers, f, sort_keys=True)
+                        f.close()
 
+                # perform DELETE action
+                elif combo == 'DELETE':
+                    window['-STATUSMSG-'].update("Removed: " + inputtxt, text_color='green')
+                    window.refresh()
+                    # remove streamer from yaml and multi
+                    # read in yaml, get the index of the streamer and remove from streamer list
+                    with open('streamers.yaml', 'r') as f:
+                        data_in_file = yaml.load_all(f, Loader=BaseLoader)
+                        if inputtxt in data_in_file:
+                            index_to_remove = list.index(streamers, inputtxt)
+                            streamers.remove(streamers[index_to_remove])
+                            window.refresh()
+                            f.close()
+                            # update the yaml streamer file
+                            with open('streamers.yaml', 'w') as f2:
+                                yaml.dump_all(streamers, f2, sort_keys=True)
+                                window.refresh()
+                            f2.close()
+
+                        else:
+                            window['-STATUSMSG-'].update(inputtxt + " doesn't exist in the streamer file.", text_color='red')
+
+                else:
+                    continue
+
+        # perform STARTMON action
         if event == '-STARTMON-':
+            window['-STARTMON-'].update(disabled=True)
+            window['-STOPMON-'].update(disabled=False)
+            window['-STATUSMSG-'].update("Monitoring started.", text_color='green')
             _thread.start_new_thread(new_thread, (window, event, values))
-            # TODO if button is pressed twice, disable/toggle?
+
+        # perform STOPMON action
+        if event == '-STOPMON-':
+            window['-FRAME_LIVE_TEXT-'].update("")
+            #window['-LBLIVE-'].update([])
+            window['-STATUSMSG-'].update("Monitoring stopped.", text_color='red')
+            window['-STOPMON-'].update(disabled=True)
+            window['-STARTMON-'].update(disabled=False)
+            # window.refresh()
+            _thread.start_new_thread(new_thread, (window, event, values))
+            continue
 
     window.close()
 
@@ -122,26 +168,30 @@ def starting_app():
 if __name__ == "__main__":
     # LAYOUT CREATION
     status_frame_layout = [
-        [sg.Text('App started.', key='-STATUSMSG-', size=25, text_color='black', pad=(5, 5))]
+        [sg.Text('App started.', key='-STATUSMSG-', size=47, text_color='black', pad=(3, 3))]
+    ]
+    status_addremove_layout = [
+        [sg.InputText('', size=24, key='-INPUTTXT-'), sg.Combo(['ADD', 'DELETE'], key='-COMBO-', auto_size_text=True, readonly=True, size=18),
+         sg.Button('Submit', key='-SUBMIT-', visible=True)],
     ]
 
     frame_layout = [
-        [sg.Multiline('', key="-FRAME_LIVE_TEXT-", size=(25, 10), no_scrollbar=True)],
+        [sg.Multiline('', key="-FRAME_LIVE_TEXT-", size=(25, 10), no_scrollbar=True, disabled=True)],
+        #[sg.Listbox([], key='-LBLIVE-', size=(25, 10), no_scrollbar=True)],
     ]
     main_layout = [
-        [sg.Text('Enter streamer name:', size=20), sg.Button('Add', key='-ADD-'), sg.InputText()],
+        [sg.Frame("Enter name of streamer then Add/Delete from dropdown.", status_addremove_layout, key='-FRAMEADDREMOVE-', element_justification='right')],
         [sg.Multiline("", write_only=True, key='-MULTI-', autoscroll=True, size=(25, 11),
-                      visible=True),
+                      visible=True, disabled=True),
          sg.Frame("LIVE Channels", frame_layout, key='-FRAME-', element_justification='left')],
-        [sg.Button('Update', key='-UPDATE-', visible=True),
-         sg.Button('Start Monitoring', key='-STARTMON-', visible=True),
+        [sg.Button('Start Monitoring', key='-STARTMON-', disabled=False, visible=True),
+         sg.Button('Stop Monitoring', key='-STOPMON-', disabled=True, visible=True),
          sg.Button('Close')],
         [sg.Frame("Status Message", status_frame_layout, key='-STATUSFRAME-', element_justification='left')],
     ]
     # TODO make window scalable/resizable and dynamic
-    window = sg.Window("Your Favorite LIVE Streamer Monitor", main_layout, size=(450, 350), finalize=True,
+    window = sg.Window("Your Favorite LIVE Streamer Monitor", main_layout, size=(450, 360), finalize=True,
                        icon='twitchmon.ico', border_depth=5, keep_on_top=True)  # scaling=True
     i = 0
     threads = []
-
     starting_app()
